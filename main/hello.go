0ec23cfef8967e71e4552528aaf76ac862faf65e
Andy <thoaitran93@gmail.com>
2024-08-03 23:31:46 +0700

    init Go project

---
+++ main/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"example/greetings"
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Hello("Andy")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+}
