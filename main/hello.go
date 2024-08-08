f2e1ab14b1888dd04ae1927e68ce2d22da3925d3
Andy <thoaitran93@gmail.com>
2024-08-08 23:37:04 +0700

    Completed Simple

--- main/hello.go
+++ main/hello.go
@@ -9,9 +9,13 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("Andy")
+
+	// A slice of names
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
